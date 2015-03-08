bdc433b0d02f34de45dd43a5ac6887fe2c87ecee
Ruben Roy <rubenroy2005@gmail.com>
2015-03-08 10:03:39 +0530

    n-2.0
    Change-Id: I0a2ae5b9ff8d02c7ccd4702234d340bb1cd630b6

---
+++ nameless_grouper.mk
@@ -0,0 +1,27 @@
+# Release name
+PRODUCT_RELEASE_NAME := Nexus7
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1280
+TARGET_SCREEN_WIDTH := 800
+
+# Inherit some common CM stuff.
+
+# Enhanced NFC
+$(call inherit-product, vendor/nameless/config/nfc_enhanced.mk)
+
+# Inherit device configuration
+$(call inherit-product, device/asus/grouper/full_grouper.mk)
+
+## Device identifier. This must come after all inclusions
+PRODUCT_DEVICE := grouper
+PRODUCT_NAME := nameless_grouper
+PRODUCT_BRAND := google
+PRODUCT_MODEL := Nexus 7
+PRODUCT_MANUFACTURER := asus
+
+#Set build fingerprint / ID / Product Name ect.
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasi BUILD_FINGERPRINT="google/nakasi/grouper:5.0/LRX21P/1570855:user/release-keys" PRIVATE_BUILD_DESC="nakasi-user 5.0 LRX21P 1570855 release-keys"
+include vendor/nameless/config/common.mk
+$(call inherit-product, vendor/nameless/config/apns.mk)
+PRODUCT_NAME := nameless_grouper
